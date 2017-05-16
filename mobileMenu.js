717f2649c0916bacf52859ec1d95a9cbc6e821bc
andres <ajarana@email.wm.edu>
2017-05-15 20:01:15 -0400

    Updates

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,12 +1,31 @@
 (function showMenu() {
   var mobileButton = document.getElementById("mobileButton");
   var html = document.getElementsByTagName("html");
+  var headerMenu = document.getElementById("headerMenu");
+
+  var minMenuHeight = '470';
+
 
   mobileButton.onclick = function() {
+    headerMenu.classList.add("transitionEverything");
+
     if (html[0].classList.contains("expanded")) {
-      html[0].classList.remove("expanded");
+      html[0].classList.remove("expanded", "staticHeight");
+    }
+    else if (window.innerHeight < minMenuHeight) {
+      html[0].classList.add("expanded", "staticHeight");
     } else {
       html[0].classList.add("expanded");
     }
   }
+
+  window.addEventListener("resize", function() {
+    headerMenu.classList.remove("transitionEverything");
+
+    if (window.innerHeight < minMenuHeight) {
+      html[0].classList.add("staticHeight");
+    } else {
+      html[0].classList.remove("staticHeight");
+    }
+  }, false);
 })();
